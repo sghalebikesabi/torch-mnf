e4aabdaf5bbb883e224ecc72cfae67567486068a
Sahra Ghalebikesabi <s.ghalebikesabi@gmail.com>
2023-04-17 12:21:18 +0100

    script for creating sweep bash files

---
+++ cla_sweeps.py
@@ -0,0 +1,8 @@
+for data in ['mnist', 'ionosphere', 'parkinsons']:
+    coms = ''
+    for lr in [5e-4, 1e-3, 5e-3, 1e-2]:
+        for num_hidden in [16, 32, 64]:
+            coms += "python uci_mnf.py --regress 0 --lr_0 %f --data_name %s --num_hidden %i\n" % (lr, data, num_hidden)
+                
+    with open('%s_sweeps.sh' % (data), 'w') as f:
+        f.write(coms)
